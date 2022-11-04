22badb3e58ce95481869c4ad3fc3af85db131ed3
irshad_aj <irshadaj08@gmail.com>
2022-11-04 11:48:15 +0530

    Calculate Salary Interface created

---
+++ SuperMarket.Api.Employee/Interfaces/ICalculateSalary.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Api.Employee.Interfaces
+{
+    public interface ICalculateSalary
+    {
+        public int CalculateEmployeeSalary();
+    }
+}
