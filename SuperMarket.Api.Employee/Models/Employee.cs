8219477d3a526588f5dd51a489f40e9994426593
irshad_aj <irshadaj08@gmail.com>
2022-11-04 11:02:56 +0530

    2 models created

---
+++ SuperMarket.Api.Employee/Models/Employee.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Api.Employee.Models
+{
+    public class Employee
+    {
+        public int EmployeeId {get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public string Address {get; set;}
+        public int AdharNumber {get; set;}
+        public string adharDocumnet {get; set;}
+        public bool isVerified {get; set;}
+        public string designation {get; set;}
+    }
+}
---
+++ SuperMarket.Api.Employee/Models/User.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Api.Employee.Models
+{
+    public class User
+    {
+        public int UserId {get; set;}
+        public string UserName {get; set;}
+
+        public string Password {get; set;}
+        public  string UserType {get; set;}
+
+    }
+}
