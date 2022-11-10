d3ec5bc0eefd4da922069ee8a8f8209917951b0d
irshad_aj <irshadaj08@gmail.com>
2022-11-10 16:48:57 +0530

    Madde Employee Model props optional

--- SuperMarket.Data.Employee/Models/EmployeeModel.cs
+++ SuperMarket.Data.Employee/Models/EmployeeModel.cs
@@ -4,12 +4,12 @@ namespace SuperMarket.Data.Employee.Models
     {
         
         public int EmployeeModelId {get; set;}
-        public string FirstName {get; set;}
-        public string LastName {get; set;}
-        public string Address {get; set;}
+        public string? FirstName {get; set;}
+        public string? LastName {get; set;}
+        public string? Address {get; set;}
         public int AadharNumber {get; set;}
-        public string AadharDocument {get; set;}
+        public string? AadharDocument {get; set;}
         public bool isVerified {get; set;}
-        public string Designation {get; set;}
+        public string? Designation {get; set;}
     }
 }
