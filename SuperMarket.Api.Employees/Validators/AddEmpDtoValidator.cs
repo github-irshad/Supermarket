17d4e81cc461505af785fba5741be199b58cffae
irshad_aj <irshadaj08@gmail.com>
2022-12-12 10:30:38 +0530

    Working on fluentValidation

---
+++ SuperMarket.Api.Employees/Validators/AddEmpDtoValidator.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using SuperMarket.Data.Employees.RequestModel;
+
+namespace SuperMarket.Api.Employees.Validators
+{
+    public class AddEmpDtoValidator : AbstractValidator<AddEmployeeDto>
+    {
+        public AddEmpDtoValidator()
+        {
+            RuleFor(x=>x.Email).EmailAddress();
+            RuleFor(x=>x.FirstName).NotEmpty();
+            RuleFor(x=>x.PhoneNumber).NotEmpty();
+            RuleFor(x=>x.AadharNumber).NotEmpty();
+            RuleFor(x=>x.LastName).NotEmpty();
+            RuleFor(x=>x.AadharDocument).NotEmpty();
+            RuleFor(x=>x.Designation).NotEmpty();
+            RuleFor(x=>x.Address).NotEmpty();
+            RuleFor(x=>x.Department).NotEmpty();
+        }
+    }
+}
