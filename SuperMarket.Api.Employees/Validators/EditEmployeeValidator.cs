3572c7fe0c6dda0e33ac4383f59ea04b643c2d7d
irshad_aj <irshadaj08@gmail.com>
2022-12-19 11:09:33 +0530

    master branch ok

--- SuperMarket.Api.Employees/Validators/EditEmployeeValidator.cs
+++ SuperMarket.Api.Employees/Validators/EditEmployeeValidator.cs
@@ -6,22 +6,22 @@ using FluentValidation;
 using SuperMarket.Data.Employees.RequestModel;
 
 namespace SuperMarket.Api.Employees.Validators
-{ 
-    public class EditEmployeeValidator : AbstractValidator<EditEmployee>
+{
+  public class EditEmployeeValidator : AbstractValidator<EditEmployee>
+  {
+    public EditEmployeeValidator()
     {
-        public EditEmployeeValidator()
-        {
-            
-        RuleFor(x=>x.Email).EmailAddress();
-            RuleFor(x=>x.FirstName).NotEmpty();
-            RuleFor(x=>x.PhoneNumber).NotEmpty();
-            RuleFor(x=>x.AadharNumber).NotEmpty();
-            RuleFor(x=>x.LastName).NotEmpty();
-            RuleFor(x=>x.AadharDocument).NotEmpty();
-            RuleFor(x=>x.Designation).NotEmpty();
-            RuleFor(x=>x.Address).NotEmpty();
-            RuleFor(x=>x.Department).NotEmpty();
-            // RuleFor(x=>x.UserType).NotEmpty();
-        }
+
+      RuleFor(x => x.Email).EmailAddress();
+      RuleFor(x => x.FirstName).NotEmpty();
+      RuleFor(x => x.PhoneNumber).NotEmpty();
+      RuleFor(x => x.AadharNumber).NotEmpty();
+      RuleFor(x => x.LastName).NotEmpty();
+      RuleFor(x => x.AadharDocument).NotEmpty();
+      RuleFor(x => x.Designation).NotEmpty();
+      RuleFor(x => x.Address).NotEmpty();
+      RuleFor(x => x.Department).NotEmpty();
+      // RuleFor(x=>x.UserType).NotEmpty();
     }
+  }
 }
